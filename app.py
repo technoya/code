63993b905cfc055f95ac8ab7ce1324178407fde2
codepythonprojects <113179663+technoya@users.noreply.github.com>
2023-07-03 09:16:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -45,8 +45,18 @@ def index():
 
        chatid,txt=message1(msg)
        user=txt
-       sendmessage(chatid,txt)
+       if txt=="/start":
+         sendmessage(chatid,"enter username")
+       else:
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+      
+        u_name=requests.get("https://www.instagram.com/"+user+"/?__a=1&__d=dis",headers=headers)
+        data=u_name.text
+        p=json.loads(data)
+        d=p['graphql']['user']['profile_pic_url']
        
+        tel_send_image(chatid,d)
+
        return Response('ok',status=200)
        
    else:
