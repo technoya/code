df9bdc72dcdb2ab6366b4d909503a1f5d87643c8
codepythonprojects <113179663+technoya@users.noreply.github.com>
2023-07-03 09:19:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,6 @@ def index():
        if txt=="/start":
          sendmessage(chatid,"enter username")
        else:
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
       
         u_name=requests.get("https://www.instagram.com/"+user+"/?__a=1&__d=dis",headers=headers)
         data=u_name.text
